7d7b44cf5f7317e589e3a9c753cdd55b3d63f4a7
Meepo39-1 <emarin87@yahoo.com>
2021-10-30 12:17:38 +0300

    progres bara cautare

--- app/src/main/java/com/example/tractorificare/MainActivity.java
+++ app/src/main/java/com/example/tractorificare/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tractorificare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void search(View view) {
+        SearchView searchView = (SearchView) findViewById(R.id.search_view);
+        searchView.setQueryHint("scrie ceva aici");
+        searchView.setOnQueryTextListener("aba");
     }
 }
